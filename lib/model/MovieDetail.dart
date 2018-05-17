d7499aef78ebd4c5a0b3cc5a0b4bd6ff158e2484
Bhavik Makwana <bhavik.makwana@multidots.com>
2018-05-17 17:55:18 +0530

    - added screenshot

---
+++ lib/model/MovieDetail.dart
@@ -0,0 +1,33 @@
+//class MovieDetail {
+//  final String id;
+//  final String title;
+//  final String originalTitle;
+//  final String genres;
+//  final List<String> directors;
+//  final String lengthInMinutes;
+//  final String shortSynopsis;
+//  final String synopsis;
+////  final EventImageData images;
+//  final List<String> youtubeTrailers;
+//  List<Actor> actors;
+//
+//  MovieDetail(
+//      this.id,
+//      this.title,
+//      this.originalTitle,
+//      this.genres,
+//      this.directors,
+//      this.actors,
+//      this.lengthInMinutes,
+//      this.shortSynopsis,
+//      this.synopsis,
+////      this.images,
+//      this.youtubeTrailers);
+//}
+//
+//class Actor {
+//  final String name;
+//  final String avatarUrl;
+//
+//  Actor(this.name, this.avatarUrl);
+//}
